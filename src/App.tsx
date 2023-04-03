4d22c16139b25e5bcc50df227188a62a33d1f9cd
Matthew S Monroe <msmonroe@twelvetwentyone.com>
2023-04-03 11:16:48 -0700

    Changes

--- src/App.tsx
+++ src/App.tsx
@@ -5,51 +5,29 @@ import './App.css';
 import LoggedIn from './LoggedIn';
 //import configData from '../config.json';
 
-/*
-const sleep = (time:any) => {
-  return new Promise((resolve) => {
-    setTimeout(resolve, time);
-  });
+const redirectUri = function () {
+  return process.env.NODE_ENV === 'development'
+    ? 'http://localhost:3000'
+    : 'https://pingonedemos1.azurewebsites.net';
 };
-*/
 
-//let redirectUri = function () {
-  const redirectUri = function () {
-    return process.env.NODE_ENV === 'development'
-      ? 'http://localhost:3000'
-      : 'https://pingonedemos1.azurewebsites.net';
-    /*
-  let env = '';
-  if (process.env.NODE_ENV === 'development') 
-    env = 'https://localhost:3000/';
-  else 
-    env = 'https://pingonedemos1.azurewebsites.net';
-  return env;
-  */
-  };
+const oidcConfig = {
+  onSignIn: async (user: any) => {
+    alert('You just signed in, congratz! Check out the console!');
+    console.log(user);
+    window.location.hash = '';
+  },
 
-  const oidcConfig = {
-    onSignIn: async (user: any) => {
-      alert('You just signed in, congratz! Check out the console!');
-      console.log(user);
-      window.location.hash = '';
-    },
+  authority:
+    'https://auth.pingone.com/f5561d77-6f62-4dd3-b7d9-581d9aa5ffaa/as/',
+  clientId: '03781782-2c30-4e75-b72e-9afb59c17d4d',
+  responseType: 'token id_token',
+  scope: 'openid profile',
+  redirectUri: redirectUri(),
+};
 
-    authority:
-      'https://auth.pingone.com/f5561d77-6f62-4dd3-b7d9-581d9aa5ffaa/as/',
-    clientId: '03781782-2c30-4e75-b72e-9afb59c17d4d',
-    responseType: 'token id_token',
-    scope: 'openid profile',
-    redirectUri: redirectUri(),
-  };
-
-console.log('process.env.NODE_ENV = ' + process.env.NODE_ENV);
-console.log(redirectUri());
-
-//sleep(20000);
-
-//console.log('https://localhost/');
-//console.log('https://pingonedemos1.azurewebsites.net');
+//console.log('process.env.NODE_ENV = ' + process.env.NODE_ENV);
+//console.log(redirectUri());
 
 function App() {
   return (
@@ -61,6 +39,7 @@ function App() {
           <LoggedIn />
         </header>
       </div>
+      <div></div>
     </AuthProvider>
   );
 }
